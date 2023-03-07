08377701cf596f5874fae86b595ac8516f0fbe4d
b.amiune <belen.amiune@globallogic.com>
2023-03-07 16:05:02 -0300

    resolve

---
+++ src/app/auth/services/auth-service.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor() { }
+}
